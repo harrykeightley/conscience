7de2a67c8d08218fa8a941e59f2e11fc6d976600
Brae Webb <email@braewebb.com>
2022-10-04 14:24:04 +1000

    Simulate after

--- director/feature.py
+++ director/feature.py
@@ -3,6 +3,7 @@ from behave import *
 from . import *
 from .features.keyboard import Events, press
 from .features.design import load_design_tests
+from .features.after import AfterSimulator
 
 class Feature:
     def on_load(self, suite):
@@ -31,3 +32,25 @@ class TrackKeypresses(Feature):
         def press_key(context, key):
             event = getattr(Events, key.upper())
             press(context, event)
+
+
+class MockAfter(Feature):
+    def on_start(self, context, suite):
+        context.after = AfterSimulator()
+        after_mock = MockLog(tk.Tk, "after")
+        after_mock.register(context.after.after)
+        after_mock = MockLog(tk.Widget, "after")
+        after_mock.register(context.after.after)
+        after_canel_mock = MockLog(tk.Tk, "after_cancel")
+        after_canel_mock.register(context.after.after_cancel)
+        after_canel_mock = MockLog(tk.Widget, "after_cancel")
+        after_canel_mock.register(context.after.after_cancel)
+
+        @when("one second passes")
+        def one_second_passes(context):
+            context.after.step(1000)
+
+        @when("{seconds:d} seconds pass")
+        def seconds_passes(context, seconds):
+            context.after.step(seconds * 1000)
+            
